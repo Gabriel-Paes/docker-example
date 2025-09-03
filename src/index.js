6d548980dfa87e6960cd69409c5fa4c466b1f669
Gabriel-Paes <paessouzagabriel@gmail.com>
2025-09-02 22:10:31 -0300

    Start project :tada:

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Fastify from "fastify";
+import env from "env-var";
+
+const fastify = Fastify({ logger: true });
+
+const PORT = env.get("PORT").default(3000).asInt();
+
+fastify.get("/", async (request, replay) => {
+  replay.send({ message: "OK" });
+});
+
+fastify
+  .listen({ port: PORT, host: "0.0.0.0" })
+  .then(() => console.log(`Server running on port ${PORT}`))
+  .catch((error) => {
+    fastify.log.error(error);
+    process.exit(1);
+  });
